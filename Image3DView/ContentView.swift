a8ad4c07aa6222da500893d40907d20f4348e2b2
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-05-26 16:56:20 +0530

    Initial Commit

---
+++ Image3DView/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Image3DView
+//
+//  Created by Bhumika Patel on 26/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Image3DView/Image3DViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image3DViewApp.swift
+//  Image3DView
+//
+//  Created by Bhumika Patel on 26/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Image3DViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
